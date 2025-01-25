a2ac3eb6166840c9238a93a10038be8c311fc7e7
Tony Chen <tc448@cornell.edu>
2025-01-25 08:48:11 -0500

    Day 6 part 2 done

--- src/Day6.java
+++ src/Day6.java
@@ -1,5 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Day6 {
     public static void main(String[] args) throws Exception {
@@ -25,26 +29,59 @@ public class Day6 {
             line = br.readLine();
         }
         int[][] solved = navigateArray(arr, startingRow, startingCol);
+        List<int[]> visitedCoords = new ArrayList<>();
         int visited = 0;
         for (int i = 0; i < numRows; i++){
             for (int j = 0; j < numCols; j++){
                 if (solved[i][j] == 9){
                     visited++;
+                    int[] coords = new int[2];
+                    coords[0] = i;
+                    coords[1] = j;
+                    visitedCoords.add(coords);
+                }
+            }
+        }
+        int loops = 0;
+        for (int[] coords : visitedCoords) {
+            int[][] temp = makeCopy(arr);
+            int i = coords[0];
+            int j = coords[1];
+            temp[i][j] = 1;
+            try {
+                navigateArray(temp, startingRow, startingCol);
+            } catch (Exception e) {
+                if (e.getMessage().equals("loop")) {
+                    loops++;
                 }
             }
         }
         System.out.println("1: " + visited);
+        System.out.println("2: " + loops);
     }
 
-    // Part 1
-    public static int[][] navigateArray(int[][] arr, int startingRow, int startingCol){
-        int[][] solved = arr.clone();
+    // Part 1 + Part 2
+    public static int[][] navigateArray(int[][] arr, int startingRow, int startingCol) throws Exception{
+        int[][] solved = makeCopy(arr);
         int row = startingRow;
         int col = startingCol;
         solved[row][col] = 9;
         String direction = "up";
         boolean run = true;
+        Map<String, List<String>> visited = new HashMap<>();
         while (run){
+            List<String> visitedDirections = visited.get(row + "," + col);
+            if (visitedDirections == null){
+                visitedDirections = new ArrayList<>();
+                visitedDirections.add(direction);
+            }
+            else if (!visitedDirections.contains(direction)){
+                visitedDirections.add(direction);
+            }
+            else{
+                throw new Exception("loop");
+            }
+            visited.put(row + "," + col, visitedDirections);
             switch(direction) {
                 case "up":
                     int upRow = row - 1;
@@ -57,7 +94,7 @@ public class Day6 {
                     }
                     else{
                         row--;
-                        arr[row][col] = 9;
+                        solved[row][col] = 9;
                     }
                     break;
                 case "down":
@@ -71,7 +108,7 @@ public class Day6 {
                     }
                     else{
                         row++;
-                        arr[row][col] = 9;
+                        solved[row][col] = 9;
                     }
                     break;
                 case "left":
@@ -85,7 +122,7 @@ public class Day6 {
                     }
                     else{
                         col--;
-                        arr[row][col] = 9;
+                        solved[row][col] = 9;
                     }
                     break;
                 case "right":
@@ -99,7 +136,7 @@ public class Day6 {
                     }
                     else{
                         col++;
-                        arr[row][col] = 9;
+                        solved[row][col] = 9;
                     }
                     break;
             }
@@ -108,5 +145,16 @@ public class Day6 {
     }
     public static boolean checkOutOfBounds(int[][] arr, int row, int col){
         return row < 0 || row >= arr.length || col < 0 || col >= arr[0].length;
+    }
+    public static int[][] makeCopy(int[][] arr){
+        int rows = arr.length;
+        int cols = arr[0].length;
+        int[][] res = new int[rows][cols];
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                res[i][j] = arr[i][j];
+            }
+        }
+        return res;
     }
 }
